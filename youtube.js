19c1722b2c300d20d179ef4dbb4f2d13aae6ecd4
Raibaz <raibaz@gmail.com>
2012-03-30 15:06:12 +0200

    Huge bugfix for youtube, removing the origin from the embed url makes autoplay work (it didn't)

--- youtube.js
+++ youtube.js
@@ -38,7 +38,7 @@ var youtube = {
 				service_name: youtube.name,
 				service_id: entry.media$group.yt$videoid.$t,
 				title: entry.media$group.media$title.$t,
-				code: '<iframe id="youtube-player" class="youtube-player" type="text/html" height="350" src="http://www.youtube.com/embed/' + entry.media$group.yt$videoid.$t + '?enablejsapi=1&origin=' + escape(window.location.origin) +'" frameborder="0"></iframe>'
+				code: '<iframe id="youtube-player" type="text/html" height="350" src="http://www.youtube.com/embed/' + entry.media$group.yt$videoid.$t + '?enablejsapi=1" frameborder="0"></iframe>'
 			}
 			callback(resp);
 		});
